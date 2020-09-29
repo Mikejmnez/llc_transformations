2e8948c05dd057dd4fa63dd29300d6eda1d8ef5f
Miguel Jimenez-Urias <mjimen17@jh.edu>
2020-09-29 14:14:06 -0400

    initialize and load data

--- tests/test_llcrearrange.py
+++ tests/test_llcrearrange.py
@@ -1,1 +1,14 @@
-#tests for llc_rearrange.py
+# tests for llc_rearrange.py
+import oceanspy as ospy
+import sys
+sys.path.append('/Users/Mikejmnez/llc_transformations/llc_rearrange/')
+from LLC_rearrange import LLCtransformation as LLC
+from LLC_rearrange import make_chunks, make_array, drop_size, pos_chunks, chunk_sizes, face_connect, arct_connect
+from LLC_rearrange import Dims
+
+# load sample dataset
+url = '/Users/Mikejmnez/Desktop/JHU/Poseidon/Local_LLC90.yaml'
+od = ospy.open_oceandataset.from_catalog('LLClocal', url)
+od._ds = od._ds.drop({'k', 'k_p1', 'k_u', 'k_l'})
+
+
